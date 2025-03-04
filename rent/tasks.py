237e01505253720d7662617714598895b0d53974
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-03-04 14:33:08 +0700

    Chat id in tg update

--- rent/tasks.py
+++ rent/tasks.py
@@ -8,23 +8,38 @@ TELEGRAM_CHAT_ID = "661612764"
 @shared_task
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML",
-    }
-    requests.post(url, data=payload)
+
+    chat_ids = ["661612764", "777759367", "882495943"]
+
+    responses = []
+    for chat_id in chat_ids:
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        response = requests.post(url, data=payload)
+        responses.append(response.json())
+
+    return responses
 
 
 @shared_task
 def send_telegram_feedback(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML",
-    }
-    requests.post(url, data=payload)
+    chat_ids = ["661612764", "777759367", "882495943"]
+
+    responses = []
+    for chat_id in chat_ids:
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        response = requests.post(url, data=payload)
+        responses.append(response.json())
+
+    return responses
 
 
 @shared_task
